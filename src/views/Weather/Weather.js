ee6670565beb997ca01148c0c94cb44188e21212
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-05-03 14:07:07 +0300

    reafctor Weather

--- src/views/Weather/Weather.js
+++ src/views/Weather/Weather.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
-import getIcons from '../../utils/weatherIcons';
+import React from 'react';
+import WeatherIcons from '../WeatherIcons';
 
 import './Weather.css';
 
-export default class Weather extends Component {
-  constructor(props) {
-    super(props);
+const Weather = ({ weatherResponse, city }) => {
+  if (!weatherResponse) return '';
 
-    this.host = document.createElement('div');
-    this.host.classList.add('weather__container');
-  }
+  const temp = `${weatherResponse.main.temp.toFixed(0)}\xB0`;
+  const { weather } = weatherResponse;
 
-  render() {
-    if (!this.props.weatherResponse) return '';
+  return (
+    <div className="weather">
+      <h2 className="weather__city">{city}</h2>
+      <div className="weather__icon">{weather.map(WeatherIcons)}</div>
+      <div className="weather__temp">{temp}</div>
+    </div>
+  );
+};
 
-    const { weatherResponse, city } = this.props;
-    const icons = getIcons(weatherResponse.weather);
-    const temp = weatherResponse.main.temp.toFixed(0);
-
-    return `
-			<div class="weather">
-				<h2 class="weather__city">${city}</h2>
-				<div class="weather__icon">${icons}</div>
-				<div class="weather__temp">${temp}\xB0</div>
-			</div>
-		`;
-  }
-}
+export default Weather;
