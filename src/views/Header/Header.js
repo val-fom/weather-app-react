b37dc9de6aaaafb45cd96b22a59f5f25c34f2506
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-05-07 12:18:17 +0300

    add react logo

--- src/views/Header/Header.js
+++ src/views/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import logo from './logo.svg';
 import './Header.css';
 
 const Header = () => (
-  <header>
+  <header className="header">
     <h1 className="header__heading">Weather App</h1>
+    <img
+      src={logo}
+      className="header__react-logo"
+      alt="react-logo"
+      title="react version"
+    />
   </header>
 );
 
