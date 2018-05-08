90923281aa45a5b116d3fa943029a8c00112f806
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-05-08 12:50:14 +0300

    remove useless container

--- src/components/presentational/Forecast/Forecast.js
+++ src/components/presentational/Forecast/Forecast.js
@@ -7,16 +7,14 @@ const Forecast = ({ forecastResponse }) => {
   if (!forecastResponse) return <div className="forecast__container" />;
 
   return (
-    <div className="forecast__container">
-      <div className="forecast__wrapper">
-        <div className="forecast__header">
-          <span />
-          <h2>24/3h forecast</h2>
-          <span />
-        </div>
-        <div className="forecast">
-          {forecastResponse.list.map(ThreeHourForecast)}
-        </div>
+    <div className="forecast__wrapper">
+      <div className="forecast__header">
+        <span />
+        <h2>24/3h forecast</h2>
+        <span />
+      </div>
+      <div className="forecast">
+        {forecastResponse.list.map(ThreeHourForecast)}
       </div>
     </div>
   );
